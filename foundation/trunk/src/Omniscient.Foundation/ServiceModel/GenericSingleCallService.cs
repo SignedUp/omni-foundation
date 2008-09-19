770bc06f2ba2596509ce4935e04b1fa0accd5498
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2008-09-19 01:52:33 +0000

    Added two generic service implementations

---
+++ foundation/trunk/src/Omniscient.Foundation/ServiceModel/GenericSingleCallService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Omniscient.Foundation.ServiceModel
+{
+    public class GenericSingleCallService<TContract, TImplementation>: IService<TContract>
+        where TImplementation: TContract, new()
+    {
+        public TContract GetImplementation()
+        {
+            return new TImplementation();
+        }
+
+        public string Name
+        {
+            get { return typeof(TContract).Name; }
+        }
+
+        public Type ContractType
+        {
+            get { return typeof(TContract); }
+        }
+    }
+}
---
+++ foundation/trunk/src/Omniscient.Foundation/ServiceModel/GenericSingletonService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Omniscient.Foundation.ServiceModel
+{
+    public class GenericSingletonService<TContract, TImplementation>: IService<TContract>
+        where TImplementation: TContract, new()
+    {
+        private TImplementation _singleton;
+
+        public GenericSingletonService()
+        {
+            _singleton = new TImplementation();
+        }
+
+        public TContract GetImplementation()
+        {
+            return _singleton;
+        }
+
+        public string Name
+        {
+            get { return typeof(TContract).Name; }
+        }
+
+        public Type ContractType
+        {
+            get { return typeof(TContract); }
+        }
+    }
+}
