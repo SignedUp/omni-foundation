beb7d3df7c4bd5d37120a61befc8fa675c82e47b
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2008-09-22 20:05:34 +0000

    Added entity controller store

---
+++ foundation/trunk/src/Omniscient.Foundation.Data/EntityControllerStore.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Omniscient.Foundation.Data
+{
+    /// <summary>
+    /// Default implementation for <see cref="IEntityControllerStore"/>.
+    /// </summary>
+    public class EntityControllerStore: IEntityControllerStore
+    {
+        private Dictionary<Type, object> _controllers;
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        public EntityControllerStore()
+        {
+            _controllers = new Dictionary<Type, object>();
+        }
+
+        #region IEntityControllerStore Members
+
+        /// <summary>
+        /// Registers an instance of an entity controller.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of entity controlled by <paramref name="controller"/>.</typeparam>
+        /// <param name="controller">An instance of an entity controller.</param>
+        public void Register<TEntity>(IEntityController<TEntity> controller) 
+            where TEntity : IEntity, new()
+        {
+            _controllers.Add(typeof(TEntity), controller);
+        }
+
+        /// <summary>
+        /// Gets an instance of an entity controller.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of entity to get a controller for.</typeparam>
+        /// <returns>An entity controller if one exists in the store; otherwise, returns null.</returns>
+        public IEntityController<TEntity> Get<TEntity>() 
+            where TEntity : IEntity, new()
+        {
+            if (!_controllers.ContainsKey(typeof(TEntity))) return null;
+            return (IEntityController<TEntity>)_controllers[typeof(TEntity)];
+        }
+
+        #endregion
+    }
+}
---
+++ foundation/trunk/src/Omniscient.Foundation.Data/IEntityControllerStore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Omniscient.Foundation.Data
+{
+    /// <summary>
+    /// Defines a contract for an Entity Controller store.  Stores instances of entity controllers.
+    /// </summary>
+    public interface IEntityControllerStore
+    {
+        /// <summary>
+        /// Registers an instance of an entity controller.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of entity controlled by <paramref name="controller"/>.</typeparam>
+        /// <param name="controller">An instance of an entity controller.</param>
+        void Register<TEntity>(IEntityController<TEntity> controller) where TEntity: IEntity, new();
+        
+        /// <summary>
+        /// Gets an instance of an entity controller.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of entity to get a controller for.</typeparam>
+        /// <returns>An entity controller if one exists in the store; otherwise, returns null.</returns>
+        IEntityController<TEntity> Get<TEntity>() where TEntity: IEntity, new();
+    }
+}
