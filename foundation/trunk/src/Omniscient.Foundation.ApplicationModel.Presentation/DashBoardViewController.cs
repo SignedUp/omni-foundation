483c3912a3024fb4b9fead9a9492da95dc1f8837
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2008-09-24 21:02:00 +0000

    Added an extender to the dashboard view controller

--- foundation/trunk/src/Omniscient.Foundation.ApplicationModel.Presentation/DashBoardViewController.cs
+++ foundation/trunk/src/Omniscient.Foundation.ApplicationModel.Presentation/DashBoardViewController.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Omniscient.Foundation.ApplicationModel.Modularity;
 
 namespace Omniscient.Foundation.ApplicationModel.Presentation
 {
-    public abstract class DashBoardViewController : IViewController
+    public class DashBoardViewController : IViewController
     {
         private IView _current;
         private DashBoard _dash;
+        private IExtensionPort<IDashBoardViewControllerExtenderContract> _extensionPort;
 
         public DashBoardViewController(DashBoard dash)
         {
             _dash = dash;
+            _extensionPort = new ExtensionPortBase<IDashBoardViewControllerExtenderContract>();
+            ApplicationManager.Current.ObjectContainer.Register(_extensionPort);
         }
 
 
@@ -21,18 +25,19 @@ namespace Omniscient.Foundation.ApplicationModel.Presentation
         public IView OpenView(IModel model)
         {
             IView view;
-            view = GetView(model);
-            
-            if (view == null)
-                return null;
+            foreach (IExtender<IDashBoardViewControllerExtenderContract> extender in _extensionPort.Extenders)
+            {
+                view = extender.GetImplementation().GetView(model);
+                if (view != null) continue;
+            }
 
-            view.Model = model;
+            if (view == null) return null;
+            view.Model = model;            
             _dash.OpenPanel(view);
             
+            CurrentView = view;
             return view;
         }
-
-        protected abstract IView GetView(IModel model);
 
         public IView CurrentView 
         {
