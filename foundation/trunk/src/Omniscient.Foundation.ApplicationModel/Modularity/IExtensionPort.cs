860782a05476295fced2fe2d07ac541fddd41356
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2008-09-24 20:26:04 +0000

    Added support for extension ports.

---
+++ foundation/trunk/src/Omniscient.Foundation.ApplicationModel/Modularity/IExtensionPort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Omniscient.Foundation.ApplicationModel.Modularity
+{
+    public interface IExtensionPort
+    {
+        Type ContractType { get; }
+    }
+
+    public interface IExtensionPort<TContract> : IExtensionPort
+    { 
+        void Register(IExtender<TContract> extender);
+        IEnumerable<IExtender<TContract>> Extenders { get; }
+    }
+}
