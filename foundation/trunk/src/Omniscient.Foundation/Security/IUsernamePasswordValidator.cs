e2d64e9a378b17324f33a10175b0411df1a2ef74
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2008-11-14 20:04:06 +0000

    AddAdded IUsernamePasswordValidator

---
+++ foundation/trunk/src/Omniscient.Foundation/Security/IUsernamePasswordValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Omniscient.Foundation.Security
+{
+    public interface IUsernamePasswordValidator
+    {
+        void Validate(string username, string password);
+    }
+}
