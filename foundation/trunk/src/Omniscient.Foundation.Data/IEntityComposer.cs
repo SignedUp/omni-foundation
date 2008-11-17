e9b4e32a77018e76b1c75c54492eff5af489ef04
rdolgov <rdolgov@f61eccee-802f-11dd-8b6f-79bb0735e950>
2008-11-17 22:30:47 +0000

    Added Load() to IEntityComposer.

--- foundation/trunk/src/Omniscient.Foundation.Data/IEntityComposer.cs
+++ foundation/trunk/src/Omniscient.Foundation.Data/IEntityComposer.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Omniscient.Foundation.Data.ObjectQuery;
 
 namespace Omniscient.Foundation.Data
 {
     interface IEntityComposer<TEntity> where TEntity: IEntity
     {
+        IEntityList<TEntity> Load(IEntityComposer<IEntity> composer, OQuery<TEntity> query);
     }
 }
