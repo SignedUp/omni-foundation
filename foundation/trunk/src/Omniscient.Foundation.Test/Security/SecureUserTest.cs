0f797cf9cc75e2d901acaffe02f8e74f98f0c53e
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2008-11-27 20:02:23 +0000

    Added SecureUser

---
+++ foundation/trunk/src/Omniscient.Foundation.Test/Security/SecureUserTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Security.Principal;
+
+namespace Omniscient.Foundation.Security
+{
+    [TestFixture()]
+    public class SecureUserTest
+    {
+        [Test()]
+        public void TestCreate()
+        {
+            SecureUser u = new SecureUser(new GenericIdentity("test"), new string[]{});
+        }
+
+        [Test()]
+        public void TestIsInRole()
+        {
+            SecureUser u = new SecureUser(new GenericIdentity("test"), new string[] { "role1", "role2" });
+            Assert.IsTrue(u.IsInRole("role1"));
+            Assert.IsTrue(u.IsInRole("role2"));
+            Assert.IsFalse(u.IsInRole("role3"));
+        }
+    }
+}
---
+++ foundation/trunk/src/Omniscient.Foundation/Security/SecureUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Principal;
+
+namespace Omniscient.Foundation.Security
+{
+    public class SecureUser : GenericPrincipal
+    {
+        public SecureUser(IIdentity identity, string[] roles) : base(identity, roles) { }
+    }
+}
