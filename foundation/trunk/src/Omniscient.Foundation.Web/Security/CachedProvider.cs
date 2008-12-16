f1fd662fb90f9abbcbf1aeb403309783c84bf709
rdolgov <rdolgov@f61eccee-802f-11dd-8b6f-79bb0735e950>
2008-12-16 20:52:40 +0000

    Before test.

--- foundation/trunk/src/Omniscient.Foundation.Web/Security/CachedProvider.cs
+++ foundation/trunk/src/Omniscient.Foundation.Web/Security/CachedProvider.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Web.Security;
 using System.Collections;
 using Omniscient.Foundation.Security;
+using Omniscient.Foundation.Logging;
+using Omniscient.Foundation.ApplicationModel;
 
 namespace Omniscient.Foundation.Web.Security
 {
@@ -10,6 +12,7 @@ namespace Omniscient.Foundation.Web.Security
     {
         private RoleProvider _provider;
         private Hashtable _cache;
+        private ILogger _logger;
 
         public override string ApplicationName
         {
@@ -29,16 +32,28 @@ namespace Omniscient.Foundation.Web.Security
         {
             _provider = provider;
             _cache = new Hashtable();
+            ILoggingService service;
+            service = ApplicationManager.Current.ServiceProvider.GetService<ILoggingService>();
+            if (service == null) return;
+            _logger = service.GetLogger(this.GetType());
+        }
+
+        protected void LogDebug(object message)
+        {
+            if (_logger == null) return;
+            _logger.Debug(message);
         }
 
         public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
         {
+            LogDebug("Entering CachedProvider.Initialize");
             base.Initialize(name, config);
             _provider.Initialize("CachedRoleProvider", config);
         }
 
         public override void AddUsersToRoles(string[] usernames, string[] roleNames)
         {
+            LogDebug("Entering CachedProvider.AddUsersToRoles");
             foreach (string username in usernames)
             {
                 if (_cache.ContainsKey(username))
@@ -58,11 +73,13 @@ namespace Omniscient.Foundation.Web.Security
 
         public override void CreateRole(string roleName)
         {
+            LogDebug("Entering CachedProvider.CreateRole");
             _provider.CreateRole(roleName);
         }
 
         public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
         {
+            LogDebug("Entering CachedProvider.DeleteRole");
             bool result;
             result = _provider.DeleteRole(roleName, throwOnPopulatedRole);
 
@@ -71,6 +88,7 @@ namespace Omniscient.Foundation.Web.Security
 
         public override string[] FindUsersInRole(string roleName, string usernameToMatch)
         {
+            LogDebug("Entering CachedProvider.FindUsersInRole");
             string[] users;
             users = _provider.FindUsersInRole(roleName, usernameToMatch);
 
@@ -79,6 +97,7 @@ namespace Omniscient.Foundation.Web.Security
 
         public override string[] GetAllRoles()
         {
+            LogDebug("Entering CachedProvider.GetAllRoles");
             string[] roles;
             roles = _provider.GetAllRoles();
 
@@ -87,6 +106,7 @@ namespace Omniscient.Foundation.Web.Security
 
         public override string[] GetRolesForUser(string username)
         {
+            LogDebug("Entering CachedProvider.GetRolesForUser");
             string[] roles;
 
             if (_cache.ContainsKey(username))
@@ -104,6 +124,7 @@ namespace Omniscient.Foundation.Web.Security
 
         public override string[] GetUsersInRole(string roleName)
         {
+            LogDebug("Entering CachedProvider.GetUsersInRole");
             string[] users;
             users = _provider.GetUsersInRole(roleName);
 
@@ -112,6 +133,7 @@ namespace Omniscient.Foundation.Web.Security
 
         public override bool IsUserInRole(string username, string roleName)
         {
+            LogDebug("Entering CachedProvider.IsUserInRole");
             bool result;
             List<string> roles;
 
@@ -139,6 +161,7 @@ namespace Omniscient.Foundation.Web.Security
 
         public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
         {
+            LogDebug("Entering CachedProvider.RemoveUsersFromRoles");
             foreach (string username in usernames)
             {
                 if (_cache.Contains(username))
@@ -155,6 +178,7 @@ namespace Omniscient.Foundation.Web.Security
 
         public override bool RoleExists(string roleName)
         {
+            LogDebug("Entering CachedProvider.RoleExists");
             bool result;
             result = _provider.RoleExists(roleName);
 
@@ -165,6 +189,7 @@ namespace Omniscient.Foundation.Web.Security
 
         string[] IRoleProvider.GetRolesForUser(string username)
         {
+            LogDebug("Entering CachedProvider.GetRolesForUser");
             return GetRolesForUser(username);
         }
 
