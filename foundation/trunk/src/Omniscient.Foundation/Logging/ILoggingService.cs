1f1d84d330e74c4451e88c67506ff8c5068f9e3d
rdolgov <rdolgov@f61eccee-802f-11dd-8b6f-79bb0735e950>
2008-12-16 21:05:41 +0000

    Missing Logging folder.

---
+++ foundation/trunk/src/Omniscient.Foundation/Logging/ILoggingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Omniscient.Foundation.Logging
+{
+    public interface ILoggingService
+    {
+        ILogger GetLogger(string name);
+        ILogger GetLogger(Type logSource);
+    }
+}
