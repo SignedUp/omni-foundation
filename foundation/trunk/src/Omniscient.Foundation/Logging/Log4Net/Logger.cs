794fa98bfd0cbbaa4051231f291bf5dedb8db53b
rdolgov <rdolgov@f61eccee-802f-11dd-8b6f-79bb0735e950>
2008-12-16 21:05:41 +0000

    Missing Logging folder.

---
+++ foundation/trunk/src/Omniscient.Foundation/Logging/Log4Net/Logger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Omniscient.Foundation.Logging.Log4Net
+{
+    class Logger: ILogger
+    {
+        private log4net.ILog _logger;
+
+        public Logger(log4net.ILog logger)
+        {
+            _logger = logger;
+        }
+
+        #region ILogger Members
+
+        public void Debug(object message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void Info(object message)
+        {
+            _logger.Info(message);
+        }
+
+        public void Error(object message)
+        {
+            _logger.Error(message);
+        }
+
+        public void Fatal(object message)
+        {
+            _logger.Fatal(message);
+        }
+
+        #endregion
+    }
+}
