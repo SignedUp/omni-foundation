ea25f5d169def1788149d8f57e1143f4d73f69cf
rdolgov <rdolgov@f61eccee-802f-11dd-8b6f-79bb0735e950>
2008-12-23 17:09:17 +0000

    Creating the tag for version 1.0.0.0

---
+++ contrib/tags/1.0.0.0/src/Omniscient.Foundation.Contrib/Data/MySql/ConnectivityServiceImpl.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using Omniscient.Foundation.Contrib.Data.MySql.Configuration;
+
+namespace Omniscient.Foundation.Contrib.Data.MySql
+{
+    internal class ConnectivityServiceImpl: IConnectivity
+    {
+        private Connectivity _config;
+
+        public ConnectivityServiceImpl(Connectivity config) 
+        {
+            _config = config;
+        }
+
+        #region IConnectivity Members
+
+        public MySqlConnection CreateConnection()
+        {
+            return new MySqlConnection(_config.ConnectionString);
+        }
+
+        #endregion
+    }
+}
---
+++ contrib/tags/1.0.0.0/src/Omniscient.Foundation.Contrib/Logging/Log4Net/LoggingService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Omniscient.Foundation.Logging;
+using Omniscient.Foundation.ServiceModel;
+
+namespace Omniscient.Foundation.Contrib.Logging.Log4Net
+{
+    class LoggingService: ServiceBase<ILoggingService>, ILoggingService, IConfigurable
+    {
+        #region ILoggingService Members
+
+        public ILogger GetLogger(string name)
+        {
+            return new Logger(log4net.LogManager.GetLogger(name));
+        }
+
+        public ILogger GetLogger(Type logSource)
+        {
+            return new Logger(log4net.LogManager.GetLogger(logSource));
+        }
+
+        #endregion
+
+        #region IConfigurable Members
+
+        public void Configure(System.Xml.XmlElement config)
+        {
+            log4net.Config.XmlConfigurator.Configure(config);
+        }
+
+        #endregion
+
+        public override ILoggingService GetImplementation()
+        {
+            return this;
+        }
+    }
+}
