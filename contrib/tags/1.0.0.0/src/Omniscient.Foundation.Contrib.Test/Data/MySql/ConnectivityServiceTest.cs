784991edc6be4bbcfb83292055a5d0c2597257d9
rdolgov <rdolgov@f61eccee-802f-11dd-8b6f-79bb0735e950>
2008-12-23 17:09:17 +0000

    Creating the tag for version 1.0.0.0

---
+++ contrib/tags/1.0.0.0/src/Omniscient.Foundation.Contrib.Test/Data/MySql/ConnectivityServiceTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Xml;
+using NUnit.Framework;
+using Omniscient.Foundation.ApplicationModel;
+using Omniscient.Foundation.ApplicationModel.Configuration;
+using Omniscient.Foundation.Contrib.Data.MySql;
+
+namespace Omniscient.Foundation.Contrib.Test.Data.MySql
+{
+    [TestFixture()]
+    public class ConnectivityServiceTest
+    {
+        [TearDown()]
+        public void TearDown()
+        {
+            ApplicationManager.Current.CloseApplication();
+        }
+
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ConfigureFromConfigObjectNoSubConfig()
+        {
+            ApplicationConfiguration config;
+            config = new ApplicationConfiguration();
+
+            ServiceDefinition def;
+            def = new ServiceDefinition();
+            def.Contract = "Omniscient.Foundation.Data.MySql.IConnectivity, Omniscient.Foundation.Data";
+            def.Service = "Omniscient.Foundation.Data.MySql.ConnectivityService, Omniscient.Foundation.Data";
+
+            config.ServicesConfiguration.ServiceDefinitions.Add(def);
+
+            ApplicationManager.Current.StartApplication(config);
+            Assert.IsNotNull(ApplicationManager.Current.ServiceProvider.GetService<IConnectivity>());
+        }
+
+        [Test()]
+        public void ConfigureFromConfigObject()
+        {
+            ApplicationConfiguration config;
+            config = new ApplicationConfiguration();
+
+            ServiceDefinition def;
+            def = new ServiceDefinition();
+            def.Contract = "Omniscient.Foundation.Data.MySql.IConnectivity, Omniscient.Foundation.Data";
+            def.Service = "Omniscient.Foundation.Data.MySql.ConnectivityService, Omniscient.Foundation.Data";
+            config.ServicesConfiguration.ServiceDefinitions.Add(def);
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(@"<foundation.data.mysql xmlns='http://schemas.omniscient.ca/foundation/applicationConfiguration.xsd' connectionstring='' />");
+            def.Config = doc.DocumentElement;
+
+            ApplicationManager.Current.StartApplication(config);
+            Assert.IsNotNull(ApplicationManager.Current.ServiceProvider.GetService<IConnectivity>());
+        }
+    }
+}
---
+++ contrib/tags/1.0.0.0/src/Omniscient.Foundation.Contrib/Data/MySql/ConnectivityService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using Omniscient.Foundation.Contrib.Data.MySql.Configuration;
+using Omniscient.Foundation.ServiceModel;
+
+namespace Omniscient.Foundation.Contrib.Data.MySql
+{
+    internal class ConnectivityService: ServiceBase<IConnectivity>, IConfigurable
+    {
+        private ConnectivityServiceImpl _implementation;
+
+        #region IConfigurable Members
+
+        public void Configure(XmlElement config)
+        {
+            Connectivity mysqlConfig;
+            XmlSerializer ser;
+            ser = new XmlSerializer(typeof(Connectivity));
+            mysqlConfig = (Connectivity)ser.Deserialize(new XmlNodeReader(config));
+            _implementation = new ConnectivityServiceImpl(mysqlConfig);
+        }
+
+        #endregion
+
+
+        public override IConnectivity GetImplementation()
+        {
+            if (_implementation == null) throw new InvalidOperationException("Service hasn't been configured.");
+            return _implementation;
+        }
+    }
+}
