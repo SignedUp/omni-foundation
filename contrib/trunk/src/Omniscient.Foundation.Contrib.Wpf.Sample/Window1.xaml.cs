5e3129f5502b212c7d0a33fd190cc64e3c90e245
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-02-13 16:53:42 +0000

    Added the StandardCommandContextMenu to ease using the presenter.

--- contrib/trunk/src/Omniscient.Foundation.Contrib.Wpf.Sample/Window1.xaml.cs
+++ contrib/trunk/src/Omniscient.Foundation.Contrib.Wpf.Sample/Window1.xaml.cs
@@ -23,7 +23,6 @@ namespace Omniscient.Foundation.Contrib.Wpf.Sample
     public partial class Window1 : Window
     {
         private NotifyIconPresenter _presenter;
-        private ObservableHierarchicalCommandObject _cmdItem;
 
         public Window1()
         {
@@ -33,46 +32,28 @@ namespace Omniscient.Foundation.Contrib.Wpf.Sample
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             _presenter.WriteMessage(TextBoxMessage.Text);
-            _cmdItem.Children[1].Children[0].Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 2.1.1" });
-            _cmdItem.Children[1].Children[0].Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 2.1.2" });
-            _cmdItem.Children[1].Text = "hahahahahahah";
+            _presenter.LeftClickMenu.Children[1].Children[0].Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 2.1.1" });
+            _presenter.LeftClickMenu.Children[1].Children[0].Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 2.1.2" });
+            _presenter.LeftClickMenu.Children[1].Text = "hahahahahahah";
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             Stream ico;
+            ObservableHierarchicalCommandObject cmdItem;
+    
             ico = this.GetType().Assembly.GetManifestResourceStream("Omniscient.Foundation.Contrib.Wpf.Sample.IconStart.ico");
             ImageSource icon = BitmapFrame.Create(ico);
             _presenter = new NotifyIconPresenter(icon);
 
-            //_cmdItem = _presenter.CreateHierarchicalMenu(MouseButton.Left);
-            _cmdItem = new ObservableHierarchicalCommandObject();
-            _cmdItem.Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 1" });
-            _cmdItem.Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 2", Command = new MessageboxCommand("myItem 2 message") });
-            _cmdItem.Children[0].Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 1.1", Command = new MessageboxCommand("myItem 1.1 message") });
-            _cmdItem.Children[0].Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 1.2", Command = new MessageboxCommand("myItem 1.2 message") });
-            _cmdItem.Children[0].Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 1.3" });
-            _cmdItem.Children[1].Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 2.1" });
-            _cmdItem.Children[1].Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 2.2" });
-            this.ContextMenu.ItemsSource = _cmdItem.Children;
-
-            ContextMenu menu;
-            MenuItem item;
-            ContextMenuCommand cmd;
-
-            menu = new ContextMenu();
-
-            menu.Placement = System.Windows.Controls.Primitives.PlacementMode.MousePoint;
-            cmd = new ContextMenuCommand(menu);
-            _presenter.LeftClickCommand = cmd;
-
-            menu = new ContextMenu();
-            item = new MenuItem();
-            item.Header = "right click";
-            menu.Items.Add(item);
-            menu.Placement = System.Windows.Controls.Primitives.PlacementMode.MousePoint;
-            cmd = new ContextMenuCommand(menu);
-            _presenter.RightClickCommand = cmd;
+            cmdItem = _presenter.CreateHierarchicalMenu(MouseButton.Left);
+            cmdItem.Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 1" });
+            cmdItem.Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 2", Command = new MessageboxCommand("myItem 2 message") });
+            cmdItem.Children[0].Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 1.1", Command = new MessageboxCommand("myItem 1.1 message") });
+            cmdItem.Children[0].Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 1.2", Command = new MessageboxCommand("myItem 1.2 message") });
+            cmdItem.Children[0].Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 1.3" });
+            cmdItem.Children[1].Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 2.1" });
+            cmdItem.Children[1].Children.Add(new ObservableHierarchicalCommandObject() { Text = "myItem 2.2" });
         }
     }
 
