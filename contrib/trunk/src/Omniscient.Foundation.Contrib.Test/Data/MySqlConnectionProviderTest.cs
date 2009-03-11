620655922bc287e257a25e5834c9600a5844452a
rdolgov <rdolgov@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-03-11 17:39:04 +0000

    Created MsSqlConnectionProvider and MySqlConnectionProvider.

---
+++ contrib/trunk/src/Omniscient.Foundation.Contrib.Test/Data/MySqlConnectionProviderTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+using Moq;
+using NUnit.Framework;
+using Omniscient.Foundation.Data;
+
+namespace Omniscient.Foundation.Contrib.Data
+{
+    [TestFixture]
+    public class MySqlConnectionProviderTest
+    {
+        private IConnectionProvider provider;
+        private string connectionString = "Data Source=myServerAddress;Initial Catalog=myDataBase;Integrated Security=SSPI;";
+
+        [SetUp]
+        public void SetUp()
+        {
+            Mock<IDbConnection> connectionMock = new Mock<IDbConnection>();
+            connectionMock.Expect(x => x.ConnectionString).Returns(connectionString);
+            connectionMock.Expect(x => x.Open());
+            connectionMock.Expect(x => x.State).Returns(ConnectionState.Closed);
+
+            Mock<IConnectionProvider> providerMock;
+            providerMock = new Mock<IConnectionProvider>();
+            providerMock.Expect(x => x.CreateConnection()).Returns((IDbConnection)connectionMock);
+
+            provider = (IConnectionProvider)providerMock;
+        }
+
+        [Test]
+        public void TestInstance()
+        {
+            provider = new MsSqlConnectionProvider(connectionString);
+        }
+
+        [Test]
+        public void TestCreateConnection()
+        {
+            provider = new MsSqlConnectionProvider(connectionString);
+
+            object actual = provider.CreateConnection();
+
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOfType(typeof(IDbConnection), actual);
+        }
+    }
+}
