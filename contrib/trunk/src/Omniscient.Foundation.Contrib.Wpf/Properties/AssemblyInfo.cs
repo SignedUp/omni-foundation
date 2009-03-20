ddb73769fe862460e61df4e1e30552e842a7831e
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-03-20 18:42:55 +0000

    changed version numbers

--- contrib/trunk/src/Omniscient.Foundation.Contrib.Wpf/Properties/AssemblyInfo.cs
+++ contrib/trunk/src/Omniscient.Foundation.Contrib.Wpf/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("Omniscient.Foundation.Contrib.Wpf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyCompany("Omniscient Technology Inc")]
 [assembly: AssemblyProduct("Omniscient.Foundation.Contrib.Wpf")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2009")]
+[assembly: AssemblyCopyright("Copyright © Omniscient Technology Inc 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,4 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
