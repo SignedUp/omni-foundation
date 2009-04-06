801bf4efc9bcd304ab5c7a3157fb4de17deec1db
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-04-06 18:59:54 +0000

    Added navigator base classes

---
+++ foundation/trunk/src/Omniscient.Foundation.ApplicationModel/Presentation/Navigation/ForwardsClosingViewNavigator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Omniscient.Foundation.ApplicationModel.Presentation.Navigation
+{
+    public class ForwardsClosingViewNavigator: ViewNavigatorBase
+    {
+        public event EventHandler ClosedForwards;
+
+        public ForwardsClosingViewNavigator(IViewController controller) : base(controller) 
+        {
+            ClosedForwards = delegate { };
+        }
+
+        public override void NavigateTo(IView view)
+        {
+            if (view == null) throw new ArgumentNullException("view");
+            if (CurrentView == view) return;
+
+            //if the view is already registered, then simply focus on it.
+            for (int i = 0; i < RegisteredViews.Count; i++)
+            {
+                if (RegisteredViews[i] == view)
+                {
+                    int lastPos = Cursor;
+                    Cursor = i;
+                    ViewController.Focus(RegisteredViews[i]);
+                    OnCurrentPositionChanged(lastPos);
+                    return;
+                }
+            }
+
+            //this is a new view; it's never been opened before.  If we are at the end of the row, then
+            //simply add the view to the list.  Otherwise, we have to close all views in front of the current one.
+            if (!this.CanGoForward())
+            {
+                RegisteredViews.Add(view);
+                GoForward();
+            }
+            else
+            {
+                //Get the views that will be closed
+                List<IView> forwards = new List<IView>(GetForwards());
+
+                //ask the ViewController to close the views
+                if (ViewController.CloseViewRange(forwards))
+                {
+                    //remove the closed views from the list
+                    foreach (IView v in forwards) RegisteredViews.Remove(v);
+                    OnClosedForwards();
+
+                    //add the newly added item, and focus it
+                    RegisteredViews.Add(view);
+                    GoForward();
+                }
+            }
+        }
+
+        private IEnumerable<IView> GetForwards()
+        {
+            for (int i = Cursor; i < RegisteredViews.Count - 1; i++)
+                yield return RegisteredViews[i];
+        }
+
+        protected virtual void OnClosedForwards()
+        {
+            ClosedForwards(this, EventArgs.Empty);
+        }
+
+    }
+}
---
+++ foundation/trunk/src/Omniscient.Foundation.ApplicationModel/Presentation/Navigation/IViewNavigator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Omniscient.Foundation.ApplicationModel.Presentation.Navigation
+{
+    public interface IViewNavigator
+    {
+        bool CanGoBack();
+        bool CanGoForward();
+        void GoBack();
+        void GoForward();
+
+        void NavigateTo(IView v);
+        IView CurrentView { get; }
+        int Count { get; }
+        int CurrentPosition { get; }
+
+        event EventHandler<CurrentPositionChangedEventArgs> CurrentPositionChanged;
+    }
+
+    public class CurrentPositionChangedEventArgs : EventArgs
+    {
+        public CurrentPositionChangedEventArgs(int lastPositionValue)
+        {
+            LastPosition = lastPositionValue;
+        }
+
+        public int LastPosition { get; private set; }
+    }
+
+}
