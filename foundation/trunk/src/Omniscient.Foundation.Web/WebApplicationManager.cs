ae25a075c2df3e1806658b5faa5ca9d653bab699
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-04-10 15:40:47 +0000

    Added WebApplicationManager

---
+++ foundation/trunk/src/Omniscient.Foundation.Web/WebApplicationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Omniscient.Foundation.ApplicationModel;
+using System.Web;
+
+namespace Omniscient.Foundation.Web
+{
+    public class WebApplicationManager: ApplicationManager
+    {
+        public static WebApplicationManager Current
+        {
+            get
+            {
+                if (HttpContext.Current.Application["WebApplicationManager"] == null)
+                    HttpContext.Current.Application["WebApplicationManager"] = new WebApplicationManager();
+                return (WebApplicationManager)HttpContext.Current.Application["WebApplicationManager"];
+            }
+        }
+    }
+}
