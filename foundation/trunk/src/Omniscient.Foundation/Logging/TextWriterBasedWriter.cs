6d3b8e97c29d311c7ad48bcea579cb5b1d4cb05c
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-04-22 13:21:06 +0000

    Changed logging mechanism and removed unnecessary extensions.

---
+++ foundation/trunk/src/Omniscient.Foundation/Logging/TextWriterBasedWriter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Omniscient.Foundation.Logging
+{
+    public class TextWriterBasedWriter: ILogWriter
+    {
+        public TextWriterBasedWriter(TextWriter writer)
+        {
+            Writer = writer;
+        }
+
+        public LogLevel Level
+        {
+            get;
+            set;
+        }
+
+        public bool IsEnabled
+        {
+            get;
+            set;
+        }
+
+        public TextWriter Writer
+        {
+            get;
+            set;
+        }
+
+        public void Write(LogEntry entry)
+        {
+            if (!this.IsEnabled || this.Level > entry.Level || entry == null) return;
+            Writer.WriteLine(entry.ToString());
+        }
+    }
+}
