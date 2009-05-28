f893aa45046c4c37e316420247882f3415b66e5e
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-05-28 18:35:36 +0000

    added view models

---
+++ foundation/trunk/src/Omniscient.Foundation.ApplicationModel/Presentation/IListViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Omniscient.Foundation.Patterns;
+
+namespace Omniscient.Foundation.ApplicationModel.Presentation
+{
+    public interface IListViewModel: IListWrapper<IModel>
+    {
+    }
+}
---
+++ foundation/trunk/src/Omniscient.Foundation.ApplicationModel/Presentation/IViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Omniscient.Foundation.Patterns;
+
+namespace Omniscient.Foundation.ApplicationModel.Presentation
+{
+    public interface IViewModel: IObjectWrapper<IModel>
+    {
+    }
+}
