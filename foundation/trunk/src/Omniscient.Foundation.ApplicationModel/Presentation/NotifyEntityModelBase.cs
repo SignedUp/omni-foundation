41d25168aab20c247685d5b118363ef91d180d2e
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-05-28 18:26:02 +0000

    Added NotifyEntityModelBase.cs

---
+++ foundation/trunk/src/Omniscient.Foundation.ApplicationModel/Presentation/NotifyEntityModelBase.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Omniscient.Foundation.Data;
+
+namespace Omniscient.Foundation.ApplicationModel.Presentation
+{
+    public class NotifyEntityModelBase<TEntity>: EntityModelBase<TEntity>, INotifyPropertyChanged
+        where TEntity : IEntity, new()
+    {
+        private List<string> _changedProperties;
+    
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override void BeginEdit()
+        {
+            base.BeginEdit();
+            _changedProperties = new List<string>();
+        }
+
+        public override void EndEdit(bool acceptChanges)
+        {
+            base.EndEdit(acceptChanges);
+            if (acceptChanges)
+                _changedProperties.ForEach(propertyName => RaisePropertyChanged(propertyName));
+        }
+
+        protected virtual void OnValueChanged(string propertyName)
+        {
+            if (IsBeingEdited)
+            {
+                _changedProperties.Add(propertyName);
+            }
+            else
+            {
+                RaisePropertyChanged(propertyName);
+            }
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
