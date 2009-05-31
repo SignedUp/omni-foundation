4c234860349eacb31245ddc71ea3685676cb56e8
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-05-31 16:38:24 +0000

    releasing version 1.2.0

---
+++ foundation/tags/1.2.0/src/Omniscient.Foundation.ApplicationModel/Presentation/IEntityModel.cs
@@ -0,0 +1,22 @@
+﻿using Omniscient.Foundation.Data;
+using Omniscient.Foundation.ApplicationModel;
+
+namespace Omniscient.Foundation.ApplicationModel.Presentation
+{
+    /// <summary>
+    /// Represents a model.  Models are logical wrappers around entities; they define relations between entities
+    /// and are responsible for managing the state and status of entities.
+    /// </summary>
+    public interface IEntityModel : IModel, IObjectWrapper<IEntity>
+    {
+    }
+
+    /// <summary>
+    /// Represents a model.  Models are logical wrappers around entities; they define relations between entities
+    /// and are responsible for managing the state and status of entities.
+    /// </summary>
+    public interface IEntityModel<TEntity> : IModel, IObjectWrapper<TEntity>
+        where TEntity : IEntity, new()
+    {
+    }
+}
---
+++ foundation/tags/1.2.0/src/Omniscient.Foundation.Data/IConnectionProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+
+namespace Omniscient.Foundation.Data
+{
+    ///<summary>
+    /// Provides a database connection for DB access.
+    ///</summary>
+    public interface IConnectionProvider
+    {
+        ///<summary>
+        /// Creates a database connection.
+        ///</summary>
+        ///<returns>A database connection</returns>
+        IDbConnection CreateConnection();
+    }
+}
