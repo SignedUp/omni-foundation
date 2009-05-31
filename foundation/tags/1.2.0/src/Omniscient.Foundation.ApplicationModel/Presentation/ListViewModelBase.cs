c058b4e35e52f1f6af10cb3fac848fc31814b67d
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-05-31 16:38:24 +0000

    releasing version 1.2.0

---
+++ foundation/tags/1.2.0/src/Omniscient.Foundation.ApplicationModel/Presentation/ListViewModelBase.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Omniscient.Foundation.ApplicationModel.Presentation
+{
+    public class ListViewModelBase<TModel>: IListViewModel<TModel>
+        where TModel: IModel
+    {
+        #region IObjectWrapper<List<TModel>> Members
+
+        public IList<TModel> Adapt()
+        {
+            return this;
+        }
+
+        public void Wrap(IList<TModel> item)
+        {
+            if (Original != null) throw new InvalidOperationException("Models already wrapped.");
+
+            Original = item;
+        }
+
+        #endregion
+
+        protected internal IList<TModel> Original { get; set; }
+
+        #region IList<TModel> Members
+
+        public virtual int IndexOf(TModel item)
+        {
+            return Original.IndexOf(item);
+        }
+
+        public virtual void Insert(int index, TModel item)
+        {
+            Original.Insert(index, item);
+        }
+
+        public virtual void RemoveAt(int index)
+        {
+            Original.RemoveAt(index);
+        }
+
+        public virtual TModel this[int index]
+        {
+            get
+            {
+                return Original[index];
+            }
+            set
+            {
+                Original[index] = value;
+            }
+        }
+
+        #endregion
+
+        #region ICollection<TModel> Members
+
+        public virtual void Add(TModel item)
+        {
+            Original.Add(item);
+        }
+
+        public virtual void Clear()
+        {
+            Original.Clear();
+        }
+
+        public virtual bool Contains(TModel item)
+        {
+            return Original.Contains(item);
+        }
+
+        public virtual void CopyTo(TModel[] array, int arrayIndex)
+        {
+            Original.CopyTo(array, arrayIndex);
+        }
+
+        public virtual int Count
+        {
+            get { return Original.Count; }
+        }
+
+        public virtual bool IsReadOnly
+        {
+            get { return Original.IsReadOnly; }
+        }
+
+        public virtual bool Remove(TModel item)
+        {
+            return Original.Remove(item);
+        }
+
+        #endregion
+
+        #region IEnumerable<TModel> Members
+
+        public virtual IEnumerator<TModel> GetEnumerator()
+        {
+            foreach (TModel model in Original) yield return model;
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        #endregion
+    }
+}
---
+++ foundation/tags/1.2.0/src/Omniscient.Foundation.Data/ObjectQuery/LogicalGroupComposition.cs
@@ -0,0 +1,18 @@
+﻿namespace Omniscient.Foundation.Data.ObjectQuery
+{
+    /// <summary>
+    /// Logical <see cref="WhereGroup"/> composer.
+    /// </summary>
+    public enum LogicalGroupComposition
+    {
+        /// <summary>
+        /// Logical AND.
+        /// </summary>
+        And, 
+        
+        /// <summary>
+        /// Logical OR.
+        /// </summary>
+        Or
+    }
+}
