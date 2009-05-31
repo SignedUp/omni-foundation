b024119b512cfebd4c6716b8e0e82daa2854edfe
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-05-31 16:36:38 +0000

    merged release branch 1.2.0

--- foundation/trunk/src/Omniscient.Foundation.ApplicationModel/Presentation/IViewModel.cs
+++ foundation/trunk/src/Omniscient.Foundation.ApplicationModel/Presentation/IViewModel.cs
@@ -4,6 +4,4 @@ namespace Omniscient.Foundation.ApplicationModel.Presentation
 {
     public interface IViewModel : IObjectWrapper<IModel> { }
 
-    public interface IViewModel<TModel> : IObjectWrapper<TModel>
-        where TModel : IModel { }
 }
