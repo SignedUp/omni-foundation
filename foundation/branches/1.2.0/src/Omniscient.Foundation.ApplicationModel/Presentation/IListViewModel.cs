0c71092ec48725de11f348cbcd71ccb2e9586d65
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-05-31 16:41:26 +0000

    removing release branch

--- foundation/branches/1.2.0/src/Omniscient.Foundation.ApplicationModel/Presentation/IListViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Omniscient.Foundation.ApplicationModel;
-
-namespace Omniscient.Foundation.ApplicationModel.Presentation
-{
-    public interface IListViewModel : IListWrapper<IModel> { }
-
-    public interface IListViewModel<TModel> : IListWrapper<TModel>
-        where TModel : IModel { }
-}
