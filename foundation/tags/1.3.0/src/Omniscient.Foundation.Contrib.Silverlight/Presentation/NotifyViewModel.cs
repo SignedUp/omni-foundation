80615af4d8a6955d0e75318e1863f61254d33fda
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-09-11 02:09:38 +0000

    Releasing version 1.3.0 (Silverlight 3.0)

---
+++ foundation/tags/1.3.0/src/Omniscient.Foundation.Contrib.Silverlight/Presentation/NotifyViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using Omniscient.Foundation.ApplicationModel.Presentation;
+
+namespace Omniscient.Foundation.Contrib.Silverlight.Presentation
+{
+    public class NotifyViewModel<TModel> : IViewModel<TModel>, INotifyPropertyChanged where TModel : class, IModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected TModel Original { get; set; }
+
+        public TModel Adapt()
+        {
+            return default(TModel);
+        }
+
+        public void Wrap(TModel item)
+        {
+            if (Original != null)
+            {
+                throw new InvalidOperationException("Model already wrapped.");
+            }
+
+            Original = item;
+        }
+
+        protected virtual void OnValueChanged(string propertyName)
+        {
+            RaisePropertyChanged(propertyName);
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ foundation/tags/1.3.0/src/Omniscient.Foundation/ServiceModel/ServiceBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Omniscient.Foundation.ServiceModel
+{
+    ///<summary>
+    /// Base class for services.
+    ///</summary>
+    ///<typeparam name="TContract">The service's contract type.</typeparam>
+    public abstract class ServiceBase<TContract> : IService
+    {
+        #region IService Members
+
+        public string Name
+        {
+            get { return GetType().Name; }
+        }
+
+        public Type ImplementationType
+        {
+            get { return typeof(TContract); }
+        }
+
+        object IService.GetImplementation()
+        {
+            return GetImplementation();
+        }
+
+        #endregion
+
+        #region IService<TContract> Members
+
+        ///<summary>
+        /// Returns an implementation of the contract.
+        ///</summary>
+        ///<returns>An implementation of the contract.</returns>
+        public abstract TContract GetImplementation();
+
+        #endregion
+    }
+}
