bdf6ce8844ac31060f11f1795ba85381fbd913f6
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-09-11 03:08:05 +0000

    -Made IViewModel obsolete; Added MVVM.IViewModel to replace it. -Made IObjectWrapper obsolete;

--- foundation/trunk/src/Omniscient.Foundation.ApplicationModel/Presentation/IViewModel.cs
+++ foundation/trunk/src/Omniscient.Foundation.ApplicationModel/Presentation/IViewModel.cs
@@ -1,9 +1,11 @@
-﻿namespace Omniscient.Foundation.ApplicationModel.Presentation
+﻿using System;
+namespace Omniscient.Foundation.ApplicationModel.Presentation
 {
     /// <summary>
     /// IViewModel is a wrapper for the IModel which is bound to IView.
     /// </summary>
     /// <typeparam name="TModel">Type of the model to be wrapped.</typeparam>
+    [Obsolete("Use MVVM.IViewModel instead.")]
     public interface IViewModel<TModel> : IObjectWrapper<TModel> where TModel : IModel
     {
     }
---
+++ foundation/trunk/src/Omniscient.Foundation.ApplicationModel/Presentation/MVVM/IView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Omniscient.Foundation.ApplicationModel.Presentation.MVVM
+{
+    public interface IView<TModel> : Omniscient.Foundation.ApplicationModel.Presentation.IView
+        where TModel: IModel
+    {
+        IViewModel<TModel> ViewModel { get; set; }
+    }
+}
