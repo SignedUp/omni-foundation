543a11d68f742b8be1f7094754a4b6e2c4c49858
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-09-17 19:33:07 +0000

    Added ConfigurationManager to Silverlight Contrib.

---
+++ foundation/trunk/src/Omniscient.Foundation.Contrib.Silverlight/Configuration/IConfigurationSectionHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Omniscient.Foundation.Contrib.Silverlight.Configuration
+{
+    using System.Xml.Linq;
+
+    /// <summary>
+    /// Deserializes an XElement configuration element to type <typeparam name="TSection" />.
+    /// </summary>
+    /// <typeparam name="TSection">
+    /// The type of the object to be deserialized from the configuration section.
+    /// </typeparam>
+    public interface IConfigurationSectionHandler<TSection>
+    {
+        /// <summary>
+        /// Creates an object of type <typeparamref name="TSection"/> from <see cref="XElement"/> <paramref name="element"/>.
+        /// </summary>
+        /// <param name="element">The xml element containing the section.</param>
+        /// <returns>Deserialized object.</returns>
+        TSection Create(XElement element);
+    }
+}
