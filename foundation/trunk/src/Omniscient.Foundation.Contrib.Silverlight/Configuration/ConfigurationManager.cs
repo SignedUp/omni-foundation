433f6b0ce1e5df07b1d1f061bb917c3c8b61912f
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-09-17 19:33:07 +0000

    Added ConfigurationManager to Silverlight Contrib.

---
+++ foundation/trunk/src/Omniscient.Foundation.Contrib.Silverlight/Configuration/ConfigurationManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Resources;
+using System.Xml.Linq;
+
+namespace Omniscient.Foundation.Contrib.Silverlight.Configuration
+{
+    /// <summary>
+    /// Serves as a replacement to System.Configuration.ConfigurationManager for Silverlight.  
+    /// By default, searches the .xap for a file named Silverlight.config
+    /// </summary>
+    public class ConfigurationManager
+    {
+        private static readonly XDocument XDocument;
+
+        static ConfigurationManager()
+        {
+            var streamInfo = Application.GetResourceStream(new Uri("Silverlight.config", UriKind.RelativeOrAbsolute));
+            ConfigurationManager.XDocument = XDocument.Load(streamInfo.Stream);
+        }
+
+
+        /// <summary>
+        /// Reads a section from the configuration file and returns it as a deserialized object of type <typeparamref name="TSection"/>.
+        /// </summary>
+        /// <param name="sectionName">
+        /// The name of the section in the configuration file to deserialize.  That name can appear anywhere in the configuration file, but
+        /// will be used only once (if more than one elements with that name appear, only the first will be deserialized).
+        /// </param>
+        /// <typeparam name="TSection">
+        /// The type of the deserialized object that represents the section.
+        /// </typeparam>
+        /// <typeparam name="TSectionHandler">
+        /// An object of type <see cref="IConfigurationSectionHandler{TSection}"/> that handles the section.
+        /// </typeparam>
+        /// <returns>
+        /// </returns>
+        public static TSection GetSection<TSection, TSectionHandler>(string sectionName)
+            where TSectionHandler : IConfigurationSectionHandler<TSection>
+        {
+            var handler = (IConfigurationSectionHandler<TSection>) Activator.CreateInstance(typeof(TSectionHandler));
+            var elements = ConfigurationManager.XDocument.Descendants(sectionName);
+            if (elements == null) return default(TSection);
+
+            foreach (XElement item in elements)
+            {
+                return handler.Create(item);
+            }
+            
+            return default(TSection);
+        }
+    }
+}
