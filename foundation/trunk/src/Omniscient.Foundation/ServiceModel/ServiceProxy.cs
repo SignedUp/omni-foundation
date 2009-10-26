faf67064c1dfd769a2e201059d92c9adb89c0f41
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-10-26 16:55:38 +0000

    Added missing files

---
+++ foundation/trunk/src/Omniscient.Foundation/ServiceModel/ServiceProxy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml;
+using Omniscient.Foundation.ApplicationModel;
+
+namespace Omniscient.Foundation.ServiceModel
+{
+    /// <summary>
+    /// That service create a WCF communication channel
+    /// </summary>
+    /// <typeparam name="TContract"></typeparam>
+    public class ServiceProxy<TContract> : ServiceBase<TContract>, IConfigurable
+    {
+        /// <summary>
+        /// Returns an implementation of the contract.
+        /// </summary>
+        /// <returns>An implementation of the contract.</returns>
+        public override TContract GetImplementation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Configure(XmlElement config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+                                                                                              
