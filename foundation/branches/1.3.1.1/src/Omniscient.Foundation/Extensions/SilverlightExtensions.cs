ac169a3928f3af04bffa42ad62dfa7a984fa9619
davelacerte <davelacerte@f61eccee-802f-11dd-8b6f-79bb0735e950>
2009-12-14 16:28:20 +0000

    Added new log features from trunk

---
+++ foundation/branches/1.3.1.1/src/Omniscient.Foundation/Extensions/SilverlightExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Omniscient.Foundation
+{
+    public static class SilverlightExtensions
+    {
+        public static bool Exists<T>(this List<T> list, Predicate<T> predicate)
+        {
+            foreach (T item in list)
+            {
+                if (predicate(item)) return true;
+            }
+
+            return false;
+        }
+
+        public static T Find<T>(this List<T> list, Predicate<T> predicate)
+        {
+            foreach (T item in list)
+            {
+                if (predicate(item)) return item;
+            }
+
+            return default(T);
+        }
+    }
+}
